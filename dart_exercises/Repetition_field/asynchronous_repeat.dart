55f6c65aa4c9da96c93115e76dc1fd787db70d09
CupChailatte <Chaisoda864@gmail.com>
2025-10-10 19:07:33 +0200

    practiced async programming

---
+++ dart_exercises/Repetition_field/asynchronous_repeat.dart
@@ -0,0 +1,44 @@
+import 'dart:math';
+
+Future<dynamic> task(id) => //Future will return an dynamic type after 1 second delay 
+    Future.delayed(Duration(seconds: 1), () => 'Task $id complete');
+
+longOperation() =>
+    Future.delayed(Duration(seconds: 3), () => "Finished long operation!");
+
+Future<String> getData(shouldSucceed) async {
+  if (shouldSucceed) {
+    return Future.delayed(Duration(seconds: 1), () => "Success!");
+  } else {
+    throw Exception("API limit reached"); //the exception (e)
+  }
+}
+
+Future<void> main() async {
+  try {
+    final gettingData = await getData(false);
+    print(gettingData);
+  } catch (e) {
+    print("Error caught: $e");
+  }
+
+
+    longOperation(); 
+    print("Doing other work immediately");
+    await Future.delayed(Duration(seconds: 4));
+    print("operation 'test long operation' completed");
+
+
+  try {
+    final task1 = await task(1);
+    print(task1);
+    final task2 = await task(2);
+    print(task2);
+    final task3 = await task(3);
+    print(task3);
+  } catch (e) {
+    print("error something happened");
+  } finally {
+    print('All task completed');
+  }
+}
--- lib/dart_subjects/asynchornous_programming/sandbox.dart
+++ lib/dart_subjects/asynchornous_programming/sandbox.dart
@@ -1,18 +1,12 @@
-import '../basic_dart/variables.dart';
+Future<int> getUserId() => 
+  Future.delayed(Duration(milliseconds: 500), () => 42);
 
-void countdown(int n)async {
-  for (var i = n; i >= 0; i--) {
-   await Future.delayed(Duration(seconds: 1), () => print(i));
-  }
-  ;
-}
-
-void countUpp(int number) {
-  for (var i = number; i >= 0; i--) {
-    Future.delayed(Duration(seconds: i), () => print(i));
-  } 
-}
 
 Future<void> main() async {
-countdown(10); 
+  try {
+    final user = await getUserId();
+    print(user);
+  } catch (e) {
+    print("error");
+  }
 }
